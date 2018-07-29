460303e8ab84ba926399e3f154ee0ffe9b1dc3a9
ludwig <lute_p@hotmail.com>
2018-07-29 14:48:47 +0800

    1400/800

--- src/Manager.java
+++ src/Manager.java
@@ -1,35 +1,46 @@
 import java.io.Serializable;
 
 public class Manager extends Person implements Serializable{
-	public Manager() {
-		
-	}
-	
-	public Manager(int ID, String name, String gender, String email, String contactNumber) {
-		super.ID = ID;
-		super.name = name;
-		super.gender = gender;
-		super.email = email;
-		super.contactNumber = contactNumber;
-	}
+    public static String password;
+    
+    public Manager() { // Default constructor
         
-        public int getID() {
-            return ID;
-        }
-        
-        public String getName() {
-            return name;
-        }
-        
-        public String getGender() {
-            return gender;
-        }
-        
-        public String getEmail() {
-            return email;
-        }
-        
-        public String contactNumber() {
-            return contactNumber;
-        }
+    }
+    
+    public Manager(int ID, String name, String email, String contactNumber, String gender, String password) { // Overriding constructor
+        super.ID = ID;
+        super.name = name;
+        super.email = email;
+        super.contactNumber = contactNumber;
+        super.gender = gender;
+        Manager.password = password;
+    }
+    
+    public static void register() { // Default method
+        //TODO: Implement method
+    }
+    
+    public static void register(int ID, String name, String email, String contactNumber, String gender, String password) { // Overloaded method
+        // TODO: Implement method
+    }
+    
+    public int getID() {
+        return ID;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public String contactNumber() {
+        return contactNumber;
+    }
 }
